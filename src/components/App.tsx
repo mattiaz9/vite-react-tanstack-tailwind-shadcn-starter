cf97b6c7ce56b29fdd413e49e952f2b103ffd65e
Mattia Dalzocchio <mattiadalzocchio@me.com>
2021-12-17 10:34:03 +0100

    Bump dependencies

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,14 @@
 import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Home from "@pages/Home"
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   )
 }
